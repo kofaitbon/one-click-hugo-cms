781699a84e6d3a836c241abd735deebfe719e725
koji <skoji360@yahoo.co.jp>
2019-06-24 15:01:04 +0900

    add twitter timeline

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -2,7 +2,6 @@ import React from "react";
 import format from "date-fns/format";
 
 import Jumbotron from "./components/jumbotron";
-import { TwitterTimeline } from 'react-twitter-embedded-timeline';
 
 export default class PostPreview extends React.Component {
   render() {
@@ -54,6 +53,7 @@ export default class PostPreview extends React.Component {
 
               <div className="w-60-l ph2-l">
                 <img src="/img/home-about-section.jpg" alt="" className="mb3"/>
+                <a class="twitter-timeline" data-lang="ja" data-width="300" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
               </div>
             </div>
 
@@ -61,8 +61,6 @@ export default class PostPreview extends React.Component {
             <div className="tc">
               <a href="{{.buttonLink}}" className="btn raise">Read more</a>
             </div>
-
-              <TwitterTimeline widgetId="695868534455275520" />
 
           </div>
         </div>
